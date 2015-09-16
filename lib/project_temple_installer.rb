ea3f3315319799018c1cd658550f264acf921b41
kaich <chengkai1853@163.com>
2015-09-16 14:02:10 +0800

    simple function

--- lib/project_temple_installer.rb
+++ lib/project_temple_installer.rb
@@ -1,3 +1,4 @@
+require 'pathname'
 require 'xcodeproj'
 class ProjectInstaller
 
@@ -14,13 +15,34 @@ class ProjectInstaller
   end
 
   def install 
+    downloadFile
     source_group = @project.main_group.children[0]
+    add_all_to_group "#{@current_dir}/Source", source_group
+    @project.save
   end
 
-  def add_all_to_group( path ,target_group)
-    new_group = target_group.new_group(path.dirname , path) if Dir.exist? path
+  def downloadFile
+    download_command = "svn export https://github.com/kaich/ProjectTemple/trunk/ProjectTemple/Source"
+    download_podfile = "svn export https://github.com/kaich/ProjectTemple/trunk/podfile"
+    system download_command
+    system download_podfile
+  end
+
+  def add_all_to_group(path ,target_group)
+    new_group = target_group.new_group(File.basename(path), path) if Dir.exist? path
     Dir.foreach(path) do |filename| 
-       
+      next if filename == "."              # 忽略当前目录
+      next if filename == ".."             # 忽略上级目录
+      next if filename.start_with? "."     # 忽略隐藏文件
+
+      file_path = "#{path}/#{filename}" 
+      
+      if File.directory? file_path
+        add_all_to_group file_path ,new_group
+      else
+        new_group.new_reference file_path
+        puts "add #{file_path}\n"
+      end
     end
   end
 
--- ptinstall.gemspec
+++ ptinstall.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|  
-  s.name        = 'fetchapp'  
-  s.version     = '2.3.0'  
+  s.name        = 'ptinstall'  
+  s.version     = '0.0.1'  
   s.date        = '2015-05-11'
-  s.executables = %w{ fetchapp }  
+  s.executables = %w{ ptinstall }  
   s.summary     = "fetch app"  
   s.description = "fetch apple app"  
   s.authors     = ["chengkai"]  
   s.email       = 'chengkai@1853.com'  
-  s.files       = Dir["lib/*"]  +%w{ bin/fetchapp}
+  s.files       = Dir["lib/*"]  +%w{ bin/ptinstall}
   s.require_paths = %w{ lib }
-  s.homepage    = 'http://rubygems.org/gems/fetchapp'  
+  s.homepage    = 'http://rubygems.org/gems/ptinstall'  
 end  
